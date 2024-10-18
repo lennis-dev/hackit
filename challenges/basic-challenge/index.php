af8ea02488df099ce6debd4e4b519cf6f11025fd
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-10-18 21:31:35 +0545

    add: new base64 based challenge
    Signed-off-by: Aashutosh Pudasaini <nepalidude3@gmail.com>

---
+++ challenges/basic-challenge/index.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dev\Lennis\Hackit\Challenges;
+
+require_once __DIR__.'/../../core/Challenge.php';
+
+use Dev\Lennis\Hackit\Challenge;
+
+class BasicChallenge extends Challenge {
+    protected string $title = 'Basic Challenge';
+    protected string $description = 'I heard there\'s a technique for encoding files safely in the web, something 64?';
+    protected int $difficulty = 2;
+    protected string $injectJavaScript = <<<EOL
+function validate(code){
+    if(btoa(code) === "QkFTRUxPTkE="){
+        return true;
+    }else{
+        return false;
+    }
+}
+EOL;
+    protected function prepareChallenge(): void {
+        $this->setCode('BASELONA');
+    }
+}
